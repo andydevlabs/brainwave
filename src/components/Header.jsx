db30f1681f4b62942fa575f2faed175dbe08dbdb
andydevlabs <avotiana@mail.hei.school>
2025-06-17 15:40:26 +0300

    feat: list all elements in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import { brainwave } from "../assets";
+import { navigation } from "../constants";
 
 export default function Header() {
     return (
         <>
             <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90">
-                <div className="flex items-center px-5 lg:px-7.5 xl:px-10 py-4">
+                <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
                     <a className="block w-[12rem] xl:mr-8" href="#hero">
                         <img
                             src={brainwave}
@@ -15,7 +16,17 @@ export default function Header() {
                     </a>
                     <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
                         <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-                            
+                            {navigation.map((item) => (
+                                <a
+                                    key={item.id}
+                                    href={item.url}
+                                    className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                                        item.onlyMobile ? "lg:hidden" : ""
+                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                                >
+                                    {item.title}
+                                </a>
+                            ))}
                         </div>
                     </nav>
                 </div>
