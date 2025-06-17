76c3caa367036f30c466aecd7d3b10dc572d63b9
andydevlabs <avotiana@mail.hei.school>
2025-06-17 16:15:44 +0300

    feat: add new account and sign in in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
+import { useLocation } from "react-router";
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
+import Button from "./Button"
 
 export default function Header() {
+    const pathname = useLocation();
     return (
         <>
-            <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90">
+            <div className="fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90">
                 <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
                     <a className="block w-[12rem] xl:mr-8" href="#hero">
                         <img
@@ -20,15 +23,22 @@ export default function Header() {
                                 <a
                                     key={item.id}
                                     href={item.url}
-                                    className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                                    className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-n-1 ${
                                         item.onlyMobile ? "lg:hidden" : ""
-                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                                    } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                                        item.url === pathname.hash
+                                            ? "z-2 lg:text-color-1"
+                                            : "lg:text-n-1/50"
+                                    } lg:leading-5 xl:px-12`}
                                 >
                                     {item.title}
                                 </a>
                             ))}
                         </div>
                     </nav>
+
+                    <a href="#signup" className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block">New Account</a>
+                    <Button className="hidden lg:flex" href="#login">Sign in</Button >
                 </div>
             </div>
         </>
