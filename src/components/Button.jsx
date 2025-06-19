64069cefdee54e2a54db40c7faa9fdcf4a5199ac
andydevlabs <avotiana@mail.hei.school>
2025-06-19 11:41:42 +0300

    feat: implement hamburger menu

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,6 +6,7 @@ export default function Button({
     isWhite,
     children,
     px,
+    onClick
 }) {
     const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
         px || "px-7"
@@ -15,7 +16,7 @@ export default function Button({
 
     const renderButton = () => {
         return (
-            <button className={classes}>
+            <button className={classes} onClick={onClick}>
                 <span className={spanClasses}>{children}</span>
                 <ButtonSVG isWhite={isWhite} />
             </button>
